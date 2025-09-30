fe2300133baa147dac7358f003001da36e02adda
chanmin <jcm5821@gmail.com>
2025-09-30 14:57:45 +0900

    9/30

---
+++ ch10/contact.c
@@ -0,0 +1,19 @@
+#include "contact.h"
+#include <string.h>
+#include <stdio.h>
+
+Contact kim = { 0 };
+int main()
+{
+	Contact lee = { "LEE", "010-1234-5678", 1 };
+
+	Contact lee2 = { 0 };
+
+	lee2 = lee;
+
+	// lee2 = { "LEE2", "010-1234-2345" };
+	strcpy(lee2.name, "LEE2");
+	strcpy(lee2.phone, "010-1234-2345");
+
+	return 0;
+}
---
+++ ch10/contact.h
@@ -0,0 +1,6 @@
+#pragma once
+typedef struct contact {
+	char name[20];
+	char phone[20];
+	int ringtone;
+} Contact;
